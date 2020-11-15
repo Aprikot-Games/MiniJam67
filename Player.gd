7e257e67f07a8b113b3da51db217e1673138c1b0
Gecko05 <jacacent@gmail.com>
2020-11-14 19:46:49 -0600

    First commit

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const ACCELERATION = 512
+const MAX_SPEED = 64
+const FRICTION = 0.25
+const GRAVITY = 600
+const JUMP_FORCE = 240
+const AIR_FRICTION = 0.025
+
+var motion = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if x_input != 0:
+		motion.x += x_input * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		$Frogo.animation = "run"
+		if x_input > 0:
+			$Frogo.flip_h = false
+		else:
+			$Frogo.flip_h = true
+	else:
+		$Frogo.animation = "idle"
+	
+	motion.y += GRAVITY * delta;
+	
+	if is_on_floor():
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION)
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
+	else:
+		if Input.is_action_just_released("ui_up")\
+		and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, AIR_FRICTION)
+	
+	motion = move_and_slide(motion, Vector2.UP)
