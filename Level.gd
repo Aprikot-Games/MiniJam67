8f8c7ca57eb02e9e09eeb2850730b13858126162
Gecko05 <jacacent@gmail.com>
2020-11-15 09:41:02 -0600

    The background follows the camera

---
+++ Level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	$Background.position = $Player/Camera2D.get_camera_position() - Vector2(160, 100)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
